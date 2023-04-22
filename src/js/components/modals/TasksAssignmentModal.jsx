03144ad03d46a38322f88c9dbbd44bea4b85489a
Hakan <96203930+Azkhar@users.noreply.github.com>
2023-04-22 23:20:02 +0300

    Update TasksAssignmentModal.jsx

--- src/js/components/modals/TasksAssignmentModal.jsx
+++ src/js/components/modals/TasksAssignmentModal.jsx
@@ -66,7 +66,7 @@ export default function TasksAssignmentModal() {
             
             <div className="flex items-center px-5 py-3 space-x-2 border-t border-steel_blue_light rounded-b justify-end">
               {tasks_data.tasks_editable && tasks_data.admin_check.admin ? <button type="button" className="danger-btn ml-2 float-right" onClick={() => funcLoad(tasks_data.removeTask)}><i className="fa-solid fa-trash-can mr-2"></i>Görevi Tamamen Sil</button> : undefined } 
-              {tasks_data.tasks_editable && tasks_data.admin_check.admin ? <button type="button" className={tasks_data.task_steps.length < 1 ? "save-btn ml-2 float-right opacity-30 pointer-events-none" : "save-btn ml-2 float-right"} onClick={() => funcLoad(tasks_data.createOrEditTask)}><i className="fa-solid fa-handshake-simple mr-2"></i>{tasks_data.tasks_editable ? "Görev Detaylarını Onayla" : "Görev Ata"}</button>  : undefined }
+              {tasks_data.admin_check.admin ? <button type="button" className={tasks_data.task_steps.length < 1 ? "save-btn ml-2 float-right opacity-30 pointer-events-none" : "save-btn ml-2 float-right"} onClick={() => funcLoad(tasks_data.createOrEditTask)}><i className="fa-solid fa-handshake-simple mr-2"></i>{tasks_data.tasks_editable ? "Görev Detaylarını Onayla" : "Görev Ata"}</button>  : undefined }
             </div>
           </div>
         </div>
