c46a729a4bff02138f1213cbc1325496ea7d2159
Hakan <96203930+Azkhar@users.noreply.github.com>
2023-05-02 18:22:04 +0300

    Update DashboardContext.jsx

--- src/js/context/DashboardContext.jsx
+++ src/js/context/DashboardContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 import dashboardReducer from '../reducer/dashboardReducer'
 import Dashboard from "../libraries/models/Dashboard";
 
