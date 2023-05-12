c49be1e7d6cc8d2d50b895a8aa1f1281dd7dbf4a
Hakan <96203930+Azkhar@users.noreply.github.com>
2023-05-12 17:50:47 +0300

    Update HTMLs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -44,36 +44,6 @@ module.exports = {
         esprint_red: "#AC0B03",
         esprint_gray: "#1A1A18",
 
-
-        //, fogra_dark: "#0A131F",
-        //, fogra: "#0E1B2B",
-        //, oxford_blue: "#112236",
-        //, prussian_blue: "#1E3249",
-        //, indigo_dye: "#2A425B",
-        //, mn_blue: "#37526E",
-        //, queen_blue: "#426280",
-        //, shadow_blue: "#738EA9",
-        //, steel_blue: "#A3BAD2",
-        //, steel_blue_light: "#C9D6E3",
-        //, alica_blue: "#DBE4ED",
-        //, alica_blue_middle: "#E4EBF2",
-        //, alica_blue_light: "#EDF2F6",
-        //, ghost_white: "#F9FBFF",
-
-        //, purple: "#636792",
-        //, purple_light: "#9295B5",
-        //, eggplant: "#C53030",
-        //, eggplant_light: "#D34A4A",
-        //, golden: "#FBB623",
-        //, golden_light: "#FBC44B",
-        //, sea_green: "#408759",
-        //, sea_green_light: "#4EA66D",
-
-        //, modal_bg: "#0000006E",
-        //, dark_modal_bg: "#000000BF",
-        //, esprint_red: "#AC0B03",
-        //, esprint_gray: "#1A1A18"
-
       },
 
       fontFamily: {
